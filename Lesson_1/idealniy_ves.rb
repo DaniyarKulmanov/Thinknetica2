8546d7d39af8e69ba472c42d0304ccd2e25a2692
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-09-10 20:57:40 +0600

    Программа идеального веса

---
+++ Lesson_1/idealniy_ves.rb
@@ -0,0 +1,15 @@
+puts "Введите Ваш вес:"
+
+ves = Integer(gets.chomp)
+
+puts "Введите Ваш рост:"
+
+rost = Integer(gets.chomp)
+
+best_ves = rost - 110
+
+if best_ves >= 0
+  puts "Ваш идеальный вес = #{best_ves}"
+else
+  puts "Ваш вес оптимален"
+end
