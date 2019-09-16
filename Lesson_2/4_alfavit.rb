5cc8e13b128463c72fb990d55c8aacd9e5db3d39
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-09-16 16:40:15 +0600

    Vowels refactor

--- Lesson_2/4_alfavit.rb
+++ Lesson_2/4_alfavit.rb
@@ -2,8 +2,8 @@ words = ('a'..'z').to_a
 vowel = %w[a e i o u]
 vowels = {}
 
-words.each_with_index do |letter, value|
-  vowels[letter] = value + 1 if vowel.include? letter
+words.each.with_index(1) do |letter, value|
+  vowels[letter] = value if vowel.include? letter
 end
 
 print vowels
