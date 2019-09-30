0086c94b56a8f65bb80e3da82634572b18439fa3
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-09-30 12:54:47 +0600

    main_menu.rb

--- Lesson_4/include.rb
+++ Lesson_4/include.rb
@@ -5,11 +5,5 @@ require '../Lesson_4/train'
 require '../Lesson_4/passenger_train'
 require '../Lesson_4/cargo_train'
 require '../Lesson_4/station_management'
-
-def main_menu
-  puts 'Выберите пункт меню:',
-       '1. Станции',
-       '2. Позда',
-       '3. Маршруты',
-       '0. Выход'
-end
+require '../Lesson_4/trains_management'
+require '../Lesson_4/main_menu'
