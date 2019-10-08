5b9111e4969f7817842d62cf9499ae34b754d8f8
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-10-08 21:11:19 +0600

    part 1

--- Lesson_6/station.rb
+++ Lesson_6/station.rb
@@ -2,6 +2,8 @@ require_relative 'instance_counter'
 
 class Station
 
+  RUSWPORDS = /^[а-я]/
+
   @@stations = []
 
   def self.all
@@ -10,13 +12,15 @@ class Station
 
   include InstanceCounter
 
-  attr_reader :trains, :name
+  attr_reader :trains
+  attr_accessor :name
 
   def initialize(name)
     @name = name
     @trains = []
     @@stations << self
     register_instance
+    validate!
   end
 
   def arrival(train)
@@ -33,5 +37,20 @@ class Station
     puts "Поезд #{train.id} выехал со станции #{@name}"
     @trains.delete(train)
   end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  protected
+
+  def validate!
+    raise 'Название должно состоять из кириллицы' if @name !~ RUSWPORDS
+    raise 'Длина названия должна быть не меньшу 3 букв' if @name.length < 3
+  end
+
 end
 
--- Lesson_6/wagon.rb
+++ Lesson_6/wagon.rb
@@ -8,5 +8,20 @@ class Wagon
   def initialize(length, height)
     @length = length
     @height = height
+    validate!
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  protected
+
+  def validate!
+    raise 'Длинна не должна превышать 10 метров' if length > 10
+    raise 'Высота не должна превышать 3 метров' if height > 3
   end
 end
